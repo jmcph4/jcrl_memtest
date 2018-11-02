1775cbbaa605e1895420e392232e70921fe56f12
jmcph4 <jmcph4@users.noreply.github.com>
2018-11-02 17:17:26 +1000

    Make main script return explicit exit code
    This commit makes the main runner script `run.sh` return an explicit
    exit code.

--- run.sh
+++ run.sh
@@ -8,12 +8,23 @@ for filename in bin/*; do
     done
 done
 
+pass=1
+
 for filename in bin/*.err; do
     grep "no leaks are possible" $filename > /dev/null
     
     if [ $? -eq 1 ]
     then
+        pass=0
         echo $filename" FAILED"
     fi
 done
+
+# return result accordingly
+if [ $pass -eq 0 ]
+then
+    exit 1
+else
+    exit 0
+fi
 
